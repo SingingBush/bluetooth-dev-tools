a1aa3055e4ec8b6ca745c5c7a0a00405c600018a
Samael Bate <singingbush@hotmail.com>
2014-10-16 13:52:33 +0100

    adding local bluecove 2.1.1 snapshot 63

--- src/main/java/com/singingbush/bluetooth/BluetoothServer.java
+++ src/main/java/com/singingbush/bluetooth/BluetoothServer.java
@@ -12,6 +12,12 @@ public class BluetoothServer {
         final LocalDevice localDevice;
 
         try {
+            final String stack = LocalDevice.getProperty("bluecove.stack");
+            System.out.println(stack);
+            // If automatic Bluetooth Stack detection is not enough Java System property "bluecove.stack" can be used to force desired Stack Initialization.
+            // Values "widcomm", "bluesoleil" or "winsock". By default winsock is selected if available.
+            // Another property "bluecove.stack.first" is used optimize stack detection.
+            // If -Dbluecove.stack.first=widcomm then widcomm (bluecove.dll) stack is loaded first and if not available then BlueCove will switch to winsock. By default intelbth.dll is loaded first.
             localDevice = LocalDevice.getLocalDevice();
 
             //final boolean discoverable = LocalDevice.getLocalDevice().setDiscoverable(DiscoveryAgent.GIAC);
