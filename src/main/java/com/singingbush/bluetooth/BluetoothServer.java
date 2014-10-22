862e2a6e4b8ec0e9ff1bfb09af00b4682fb3afc1
Samael Bate <singingbush@hotmail.com>
2014-10-22 16:32:20 +0100

    adding enough to get started

--- src/main/java/com/singingbush/bluetooth/BluetoothServer.java
+++ src/main/java/com/singingbush/bluetooth/BluetoothServer.java
@@ -2,18 +2,24 @@
 
 package com.singingbush.bluetooth;
 
-import javax.bluetooth.BluetoothStateException;
-import javax.bluetooth.DiscoveryAgent;
-import javax.bluetooth.LocalDevice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.bluetooth.*;
+import java.io.IOException;
 
 public class BluetoothServer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BluetoothServer.class);
 
     public void start() {
         final LocalDevice localDevice;
 
         try {
-            final String stack = LocalDevice.getProperty("bluecove.stack");
-            System.out.println(stack);
+            final String stack = LocalDevice.getProperty("bluecove.stack"); // "winsock" (Microsoft), "widcomm" (Broadcom), "bluesoleil"
+//            System.out.println("stack: " + stack);
+            LOG.debug("Bluecove is using the {} stack", stack);
+
             // If automatic Bluetooth Stack detection is not enough Java System property "bluecove.stack" can be used to force desired Stack Initialization.
             // Values "widcomm", "bluesoleil" or "winsock". By default winsock is selected if available.
             // Another property "bluecove.stack.first" is used optimize stack detection.
@@ -23,9 +29,44 @@ public class BluetoothServer {
             //final boolean discoverable = LocalDevice.getLocalDevice().setDiscoverable(DiscoveryAgent.GIAC);
             // General/Unlimited Inquiry Access Code (GIAC)
 
-            System.out.printf("\nLocal Device: %s", localDevice);
+            //System.out.printf("Local Bluetooth Device:\t%s:%s\n", localDevice.getFriendlyName(), localDevice.getBluetoothAddress());
+            LOG.debug("Local Bluetooth Device: {}:{}", localDevice.getFriendlyName(), localDevice.getBluetoothAddress());
+
+            final MyDiscoveryListener listener = new MyDiscoveryListener();
+            final boolean started = localDevice.getDiscoveryAgent().startInquiry(DiscoveryAgent.GIAC, listener);
+
         } catch (final BluetoothStateException e) {
             e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("LocalCanBeFinal")
+    private class MyDiscoveryListener implements DiscoveryListener {
+        @Override
+        public void deviceDiscovered(RemoteDevice remoteDevice, DeviceClass deviceClass) {
+            String address = remoteDevice.getBluetoothAddress();
+            String name = null;
+            try {
+                name = remoteDevice.getFriendlyName(true);
+            } catch (IOException e) {
+                LOG.error("Unable to get device name", e);
+            }
+            LOG.debug("Device discovered {} : {}", address, name);
+        }
+
+        @Override
+        public void servicesDiscovered(int i, ServiceRecord[] serviceRecords) {
+
+        }
+
+        @Override
+        public void serviceSearchCompleted(int i, int i1) {
+
+        }
+
+        @Override
+        public void inquiryCompleted(int i) {
+
         }
     }
 
